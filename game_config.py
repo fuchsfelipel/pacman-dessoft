eb89f61a43bee7df5a79fe78c4bfdc55fb1b8737
Felipe Liberman Fuchs <felipe@almosthackers.net>
2021-05-23 10:47:06 -0300

    Edit GameConfig

--- game_config.py
+++ game_config.py
@@ -1,6 +1,10 @@
 from utils.vector import Vector2
 
-class GameDimensions():
+
+class GameDimensions:
+    """
+    Especificações da tela de jogo
+    """
     tile_w = 16
     tile_h = 16
     row_num = 36
@@ -8,6 +12,7 @@ class GameDimensions():
     screen_w = tile_w * col_num
     screen_h = tile_h * row_num
     screen_size = (screen_w, screen_h)
+
 
 # Cores: https://www.schemecolor.com/pac-man-game-colors.php
 class Colors:
@@ -31,13 +36,38 @@ class GameStatus:
 
 
 class Movements:
+    """
+    Define todos os movimentos possíveis com Vetores 2-D
+    """
     UP = Vector2(0, -1)
     DOWN = Vector2(0, 1)
     LEFT = Vector2(-1, 0)
     RIGHT = Vector2(1, 0)
     STOP = Vector2(0, 0)
 
+    # O método abaixo não foi transformado em uma função
+    # por uma questão de organização. Desta maneira, sinalizei-o
+    # como estático para o interpretador
+    @staticmethod
+    def reverse(direction):
+        """
+        Retorna o inverso da direção
+        :param direction: Direção
+        :type direction: Movements
+        :return: Nova direção
+        """
+        if direction is Movements.UP:
+            return Movements.DOWN
+        elif direction is Movements.DOWN:
+            return Movements.UP
+        elif direction is Movements.LEFT:
+            return Movements.RIGHT
+        elif direction is Movements.RIGHT:
+            return Movements.LEFT
 
-class Points():
-    balls = 10
 
+class Points:
+    """
+    Define os pontos de cada coisa
+    """
+    point_balls = 10
