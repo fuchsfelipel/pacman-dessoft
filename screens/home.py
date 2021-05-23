afc5eb33857a67b9d366861aab517dbc77d7cdbf
Felipe Liberman Fuchs <felipe@almosthackers.net>
2021-05-23 18:12:34 -0300

    Make homescreen logic

--- screens/home.py
+++ screens/home.py
@@ -1,5 +1,26 @@
 # PyGame
 import pygame
 
+import game_config
+
+
 class HomeScreen:
-    pass
+    def __init__(self, window):
+        # Setup geral
+        self.window = window
+
+        # Background
+        self.background = pygame.surface.Surface(game_config.GameDimensions.screen_size).convert()
+        self.background.fill(game_config.Colors.black)
+        self.window.blit(self.background, (0, 0))
+
+    def update(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_c]:
+            return game_config.GameStatus.game
+
+        elif key[pygame.K_h]:
+            return game_config.GameStatus.highScore
+
+        else:
+            return game_config.GameStatus.home
