1f8b9271405d3b55531efe073549b63eba1f288d
Felipe Liberman Fuchs <felipe@almosthackers.net>
2021-05-23 09:47:00 -0300

    Import game utils from pacmancode

---
+++ utils/stack.py
@@ -0,0 +1,29 @@
+class Stack(object):
+    """
+    Esta classe cria o tipo stack que representa uma estrutura de dado FIFO
+    Adaptado de pacmancode.com
+    """
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        if len(self.items) > 0:
+            return False
+        return True
+
+    def clear(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.isEmpty():
+            removedItem = self.items.pop(len(self.items)-1)
+            return removedItem
+        return None
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[len(self.items)-1]
+        return None
