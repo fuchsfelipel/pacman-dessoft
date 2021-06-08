276c6531779a16cc6b42202c6084000195a6bea0
kadu-ymd <81188276+kadu-ymd@users.noreply.github.com>
2021-06-08 18:47:11 -0300

    High Score screen adjusts
    Additional adjusts to High Score screen 
    - now it prints the highest score stored on 'highscore.json'

--- screens/highscore.py
+++ screens/highscore.py
@@ -6,42 +6,51 @@ import json
 
 # DIY
 import game_config
+from sprites.pacman import Pacman
+from malha import NodeGroup
 
 class HighscoreScreen:
     def __init__(self, window):
         self.window = window
         self.nodes = NodeGroup('assets/tabuleiro.txt')
         self.pont = Pacman(self.nodes).points
-       
+
     def update(self):
         # Background
         self.window.blit(pygame.image.load("assets/highscore.png"), (0, 0))
-        pygame.display.flip()
 
         # Abre o arquivo JSON para leitura
+        with open('highscore.json', 'r') as file:
+            hs = file.read()
+
         with open('score.json', 'r') as file:
-            score = file.read()
+            s = file.read()
 
         # Converte o arquivo de JSON para Python
-        pont = json.loads(score)
-        
-        # TODO: Printa a pontuação na tela
+        hs = json.loads(hs)
+        s = json.loads(s)
+
+        if hs < s:
+            hs = s
+
         white = (255, 255, 255)
         font = pygame.font.SysFont(None, 40)
-        hi = font.render(str(pont), True, white)
+        txt = font.render('High Score', True, white)
+        hi = font.render(str(hs), True, white)
+        self.window.blit(txt, (50, 288))
         self.window.blit(hi, (224, 288))
 
         # Converte o arquivo de Python para JSON
-        tojson = json.dumps(pont)
+        tojson = json.dumps(hs)
 
         # Salva o arquivo em JSON
-        with open('score.json', 'w') as arquivo_json:
+        with open('highscore.json', 'w') as arquivo_json:
             arquivo_json.write(tojson)
+
+        pygame.display.flip()
 
         key = pygame.key.get_pressed()
         if key[pygame.K_e]:
             return game_config.GameStatus.home
         else:
             return game_config.GameStatus.highScore
-
-        
