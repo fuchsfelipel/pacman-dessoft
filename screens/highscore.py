4282824c61495efa1b5f4b3a177182ac6e1b71ca
kadu-ymd <81188276+kadu-ymd@users.noreply.github.com>
2021-06-08 17:29:00 -0300

    Update highscore.py
    - Added lines that reads 'score.json' content and print the score on screen

--- screens/highscore.py
+++ screens/highscore.py
@@ -10,15 +10,38 @@ import game_config
 class HighscoreScreen:
     def __init__(self, window):
         self.window = window
-        
+        self.nodes = NodeGroup('assets/tabuleiro.txt')
+        self.pont = Pacman(self.nodes).points
+       
     def update(self):
         # Background
         self.window.blit(pygame.image.load("assets/highscore.png"), (0, 0))
         pygame.display.flip()
+
+        # Abre o arquivo JSON para leitura
+        with open('score.json', 'r') as file:
+            score = file.read()
+
+        # Converte o arquivo de JSON para Python
+        pont = json.loads(score)
+        
+        # TODO: Printa a pontuação na tela
+        white = (255, 255, 255)
+        font = pygame.font.SysFont(None, 40)
+        hi = font.render(str(pont), True, white)
+        self.window.blit(hi, (224, 288))
+
+        # Converte o arquivo de Python para JSON
+        tojson = json.dumps(pont)
+
+        # Salva o arquivo em JSON
+        with open('score.json', 'w') as arquivo_json:
+            arquivo_json.write(tojson)
 
         key = pygame.key.get_pressed()
         if key[pygame.K_e]:
             return game_config.GameStatus.home
         else:
             return game_config.GameStatus.highScore
+
         
